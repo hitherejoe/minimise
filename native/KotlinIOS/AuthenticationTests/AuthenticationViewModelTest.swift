3922ea97382e5a01c37d302a8eaed0fd1e92e77b
hitherejoe <hitherejoebirch@gmail.com>
2020-03-22 15:24:30 +0000

    Add unit tests for authentication view model

---
+++ native/KotlinIOS/AuthenticationTests/AuthenticationViewModelTest.swift
@@ -0,0 +1,39 @@
+//
+//  AuthenticationViewModelTest.swift
+//  AuthenticationTests
+//
+//  Created by Joe Birch on 21/03/2020.
+//  Copyright © 2020 Joe Birch. All rights reserved.
+//
+
+import XCTest
+@testable import SharedAuthentication
+@testable import Authentication
+
+class AuthenticationViewModelTest: XCTestCase {
+    
+    var viewModel: AuthenticationViewModel!
+
+    override func setUp() {
+        super.setUp()
+        self.viewModel = AuthenticationViewModel(authenticate: nil)
+    }
+
+    override func tearDown() { }
+    
+    func testStateDefaultsToSignUp() {
+        XCTAssertTrue(viewModel.state.authenticationMode.isKind(of: AuthenticateMode.SignUp.self))
+    }
+
+    func testSetEmailAddressEmitsExpectedEmailState() {
+        let email = "joe@birch.com"
+        viewModel.setEmailAddress(emailAddress: email)
+        XCTAssertTrue(viewModel.state.emailAddress == email)
+    }
+    
+    func testSetPasswordEmitsExpectedPasswordState() {
+        let password = "my_password"
+        viewModel.setPassword(password: password)
+        XCTAssertTrue(viewModel.state.password == password)
+    }
+}
