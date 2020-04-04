ff57c4092a9de5b61948e967dcd2bc2e719108b8
hitherejoe <hitherejoebirch@gmail.com>
2020-04-04 08:51:30 +0100

    Improve the code usage and visuals for the authentication screen

--- platform_android/authentication-ui/src/main/kotlin/co/joebirch/minimise/authentication/Content.kt
+++ platform_android/authentication-ui/src/main/kotlin/co/joebirch/minimise/authentication/Content.kt
@@ -4,17 +4,24 @@ import android.view.ViewGroup
 import androidx.compose.Composable
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
+import androidx.ui.core.Alignment
 import androidx.ui.core.Modifier
-import androidx.ui.foundation.Text
-import androidx.ui.foundation.TextField
+import androidx.ui.foundation.*
+import androidx.ui.foundation.shape.corner.CornerSize
+import androidx.ui.foundation.shape.corner.RoundedCornerShape
+import androidx.ui.graphics.Color
 import androidx.ui.input.ImeAction
 import androidx.ui.input.KeyboardType
 import androidx.ui.input.PasswordVisualTransformation
 import androidx.ui.layout.*
 import androidx.ui.material.Button
 import androidx.ui.material.CircularProgressIndicator
+import androidx.ui.material.Surface
 import androidx.ui.material.TextButton
 import androidx.ui.res.stringResource
+import androidx.ui.text.TextFieldValue
+import androidx.ui.text.TextStyle
+import androidx.ui.text.style.TextAlign
 import androidx.ui.unit.dp
 import co.joebirch.minimise.authentication.ui.R
 import co.joebirch.minimise.common_ui.observe
@@ -79,68 +86,73 @@ private fun FormContent(
     emailChanged: (String) -> Unit,
     passwordChanged: (String) -> Unit
 ) {
-    Column(
-    ) {
-        Text(
-            text = stringResource(id = R.string.title_minimise),
-            modifier = LayoutGravity.Center
-        )
-        Spacer(Modifier.preferredSize(24.dp))
-        Column(modifier = Modifier.padding(16.dp)) {
-            TextField(
-                value = emailAddress,
-                onValueChange = { emailChanged(it) },
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Next,
-                modifier = LayoutGravity.Center
-            )
-            Spacer(LayoutHeight(12.dp))
-            TextField(
-                value = password,
-                onValueChange = { passwordChanged(it) },
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done,
-                modifier = LayoutGravity.Center,
-                visualTransformation = PasswordVisualTransformation()
-            )
-            Spacer(LayoutHeight(12.dp))
-            if (authenticationMode == AuthenticateMode.SignIn) {
-                TextButton(onClick = {
-                    forgotPasswordClicked()
-                }, modifier = LayoutGravity.Center.plus(LayoutWidth.Fill)) {
-                    Text(
-                        text = stringResource(R.string.forgotten_your_password),
-                        modifier = LayoutGravity.Center.plus(
-                            LayoutPadding(16.dp)
-                        )
-                    )
+    Column(modifier = Modifier.wrapContentHeight(align = Alignment.Center)) {
+        Surface(
+            modifier = Modifier.padding(16.dp),
+            color = Color.White,
+            shape = RoundedCornerShape(CornerSize(4.dp))
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                TextField(
+                    value = TextFieldValue(text = emailAddress),
+                    onValueChange = { emailChanged(it.text) },
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next,
+                    modifier = Modifier.gravity(ColumnAlign.Center).padding(16.dp)
+                )
+                TextField(
+                    value = TextFieldValue(text = password),
+                    onValueChange = { passwordChanged(it.text) },
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done,
+                    visualTransformation = PasswordVisualTransformation(),
+                    modifier = Modifier.gravity(ColumnAlign.Center).padding(16.dp)
+                )
+                if (authenticationMode == AuthenticateMode.SignIn) {
+                    TextButton(onClick = {
+                        forgotPasswordClicked()
+                    }, modifier = Modifier.gravity(ColumnAlign.Center)) {
+                        ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
+                            Text(
+                                text = stringResource(R.string.forgotten_your_password),
+                                modifier = Modifier.gravity(ColumnAlign.Center)
+                                    .padding(top = 8.dp, bottom = 8.dp, start = 12.dp, end = 12.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
-
-        Column {
+        Spacer(Modifier.preferredHeight(32.dp))
+        Column(modifier = Modifier.gravity(ColumnAlign.Center)) {
             Button(onClick = {
                 authenticateClicked()
-            }, modifier = LayoutGravity.Center.plus(LayoutWidth.Fill)) {
-                Text(
-                    text = if (authenticationMode == AuthenticateMode.SignIn) {
-                        stringResource(R.string.sign_in)
-                    } else {
-                        stringResource(R.string.sign_up)
-                    }
-                )
+            }, modifier = Modifier.gravity(ColumnAlign.Center)) {
+                ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
+                    Text(
+                        text = if (authenticationMode == AuthenticateMode.SignIn) {
+                            stringResource(R.string.sign_in)
+                        } else {
+                            stringResource(R.string.sign_up)
+                        },
+                        modifier = Modifier.preferredSizeIn(minWidth = 220.dp)
+                    )
+                }
             }
-            Spacer(LayoutHeight(12.dp))
+            Spacer(Modifier.preferredHeight(16.dp))
             Button(onClick = {
                 authenticationModeToggled()
-            }, modifier = LayoutGravity.Center.plus(LayoutWidth.Fill)) {
-                Text(
-                    text = if (authenticationMode == AuthenticateMode.SignIn) {
-                        stringResource(R.string.no_account)
-                    } else {
-                        stringResource(R.string.existing_account)
-                    }
-                )
+            }, modifier = Modifier.gravity(ColumnAlign.Center)) {
+                ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
+                    Text(
+                        text = if (authenticationMode == AuthenticateMode.SignIn) {
+                            stringResource(R.string.no_account)
+                        } else {
+                            stringResource(R.string.existing_account)
+                        },
+                        modifier = Modifier.preferredSizeIn(minWidth = 220.dp)
+                    )
+                }
             }
         }
     }
