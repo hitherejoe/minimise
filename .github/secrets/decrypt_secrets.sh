5396b2a8b8e74567c0480605f0ef3a80b2c2fdc7
Joe Birch <hitherejoebirch@googlemail.com>
2020-04-05 21:05:14 +0100

    Update decrypt_secrets.sh

--- .github/secrets/decrypt_secrets.sh
+++ .github/secrets/decrypt_secrets.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eo pipefail
 
-gpg --quiet --batch --yes --decrypt --passphrase="${{ secrets.IOS_KEYS }}" --output ./.github/secrets/github_actions.mobileprovision.mobileprovision ./.github/secrets/github_actions.mobileprovision.gpg
-gpg --quiet --batch --yes --decrypt --passphrase="${{ secrets.IOS_KEYS }}" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/github_actions.mobileprovision.mobileprovision ./.github/secrets/github_actions.mobileprovision.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$IOS_KEYS" --output ./.github/secrets/Certificates.p12 ./.github/secrets/Certificates.p12.gpg
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 
