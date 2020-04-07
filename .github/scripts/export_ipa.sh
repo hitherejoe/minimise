a2a85c10b922e80b356813064e85599b4aeddd25
Joe Birch <hitherejoebirch@googlemail.com>
2020-04-07 13:49:32 +0100

    Update export_ipa.sh

--- .github/scripts/export_ipa.sh
+++ .github/scripts/export_ipa.sh
@@ -4,8 +4,13 @@ set -eo pipefail
 
 cd native/KotlinIOS
 
+ls
+
 xcodebuild -archivePath DerivedData/Archive/Minimise.xcarchive \
             -exportOptionsPlist KotlinIOS/exportOptions.plist \
-            -exportPath DerivedData \
+            -exportPath DerivedData/Build \
             -allowProvisioningUpdates \
             -exportArchive | xcpretty
+
+
+ls DerivedData/Build
