e6e079351c309fb5a5ce1da3dcfebb92214d0c38
hitherejoe <hitherejoebirch@gmail.com>
2020-07-26 19:05:19 +0100

    Switch out VerticalScroller for ScrollableColumn

--- platform_android/authentication-ui/src/main/kotlin/co/joebirch/minimise/authentication/Content.kt
+++ platform_android/authentication-ui/src/main/kotlin/co/joebirch/minimise/authentication/Content.kt
@@ -93,106 +93,104 @@ private fun FormContent(
             if (isLoading) {
                 CircularProgressIndicator(modifier = Modifier.gravity(Alignment.Center))
             } else {
-                VerticalScroller(modifier = Modifier.fillMaxSize()) {
-                    Column(
-                        modifier = Modifier.padding(16.dp)
-                    ) {
-                        Spacer(modifier = Modifier.preferredHeight(20.dp))
-                        Text(
-                            modifier = Modifier.preferredWidth(240.dp)
-                                .gravity(Alignment.CenterHorizontally),
-                            text = if (authenticationMode == AuthenticateMode.SignUp) {
-                                "Sign up for a Minimise account"
-                            } else "Sign in to your Minimise account",
-                            textAlign = TextAlign.Center,
-                            fontFamily = FontFamily.Monospace,
-                            style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                ScrollableColumn(
+                    modifier = Modifier.padding(16.dp).fillMaxSize()
+                ) {
+                    Spacer(modifier = Modifier.preferredHeight(20.dp))
+                    Text(
+                        modifier = Modifier.preferredWidth(240.dp)
+                            .gravity(Alignment.CenterHorizontally),
+                        text = if (authenticationMode == AuthenticateMode.SignUp) {
+                            "Sign up for a Minimise account"
+                        } else "Sign in to your Minimise account",
+                        textAlign = TextAlign.Center,
+                        fontFamily = FontFamily.Monospace,
+                        style = TextStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                    )
+                    Spacer(modifier = Modifier.preferredHeight(36.dp))
+                    Column(modifier = Modifier.padding(8.dp)) {
+                        FilledTextField(
+                            value = emailState.value,
+                            onValueChange = {
+                                emailState.value = it
+                                emailChanged(it.text)
+                            },
+                            label = {
+                                Text(text = "Email Address", fontSize = 12.sp)
+                            },
+                            keyboardType = KeyboardType.Email,
+                            imeAction = ImeAction.Next,
+                            modifier = Modifier.padding(16.dp).fillMaxWidth()
                         )
-                        Spacer(modifier = Modifier.preferredHeight(36.dp))
-                        Column(modifier = Modifier.padding(8.dp)) {
-                            FilledTextField(
-                                value = emailState.value,
-                                onValueChange = {
-                                    emailState.value = it
-                                    emailChanged(it.text)
-                                },
-                                label = {
-                                    Text(text = "Email Address", fontSize = 12.sp)
-                                },
-                                keyboardType = KeyboardType.Email,
-                                imeAction = ImeAction.Next,
-                                modifier = Modifier.padding(16.dp).fillMaxWidth()
-                            )
-                            FilledTextField(
-                                value = passwordState.value,
-                                onValueChange = {
-                                    passwordState.value = it
-                                    passwordChanged(it.text)
-                                },
-                                label = {
-                                    Text(text = "Password", fontSize = 12.sp)
-                                },
-                                visualTransformation = PasswordVisualTransformation(),
-                                keyboardType = KeyboardType.Password,
-                                imeAction = ImeAction.Done,
-                                modifier = Modifier.padding(16.dp).fillMaxWidth()
-                            )
-                            if (authenticationMode == AuthenticateMode.SignIn) {
-                                TextButton(onClick = {
-                                    forgotPasswordClicked()
-                                }) {
-                                    ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
-                                        Text(
-                                            text = stringResource(R.string.forgotten_your_password),
-                                            modifier = Modifier
-                                                .padding(
-                                                    top = 8.dp,
-                                                    bottom = 8.dp,
-                                                    start = 12.dp,
-                                                    end = 12.dp
-                                                )
-                                        )
-                                    }
+                        FilledTextField(
+                            value = passwordState.value,
+                            onValueChange = {
+                                passwordState.value = it
+                                passwordChanged(it.text)
+                            },
+                            label = {
+                                Text(text = "Password", fontSize = 12.sp)
+                            },
+                            visualTransformation = PasswordVisualTransformation(),
+                            keyboardType = KeyboardType.Password,
+                            imeAction = ImeAction.Done,
+                            modifier = Modifier.padding(16.dp).fillMaxWidth()
+                        )
+                        if (authenticationMode == AuthenticateMode.SignIn) {
+                            TextButton(onClick = {
+                                forgotPasswordClicked()
+                            }) {
+                                ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
+                                    Text(
+                                        text = stringResource(R.string.forgotten_your_password),
+                                        modifier = Modifier
+                                            .padding(
+                                                top = 8.dp,
+                                                bottom = 8.dp,
+                                                start = 12.dp,
+                                                end = 12.dp
+                                            )
+                                    )
                                 }
                             }
                         }
                     }
-                    Column(
-                        modifier = Modifier.fillMaxSize().weight(1f, fill = true),
-                        horizontalGravity = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.Bottom
-                    ) {
-                        Button(onClick = {
-                            authenticateClicked()
-                        }) {
-                            ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
-                                Text(
-                                    text = if (authenticationMode == AuthenticateMode.SignIn) {
-                                        stringResource(R.string.sign_in)
-                                    } else {
-                                        stringResource(R.string.sign_up)
-                                    },
-                                    modifier = Modifier.preferredSizeIn(minWidth = 220.dp)
-                                )
-                            }
+                }
+                Column(
+                    modifier = Modifier.fillMaxSize().weight(1f, fill = true),
+                    horizontalGravity = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Bottom
+                ) {
+                    Button(onClick = {
+                        authenticateClicked()
+                    }) {
+                        ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
+                            Text(
+                                text = if (authenticationMode == AuthenticateMode.SignIn) {
+                                    stringResource(R.string.sign_in)
+                                } else {
+                                    stringResource(R.string.sign_up)
+                                },
+                                modifier = Modifier.preferredSizeIn(minWidth = 220.dp)
+                            )
                         }
-                        Spacer(Modifier.preferredHeight(16.dp))
-                        TextButton(onClick = {
-                            authenticationModeToggled()
-                        }) {
-                            ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
-                                Text(
-                                    text = if (authenticationMode == AuthenticateMode.SignIn) {
-                                        stringResource(R.string.no_account)
-                                    } else {
-                                        stringResource(R.string.existing_account)
-                                    },
-                                    modifier = Modifier.preferredSizeIn(minWidth = 220.dp)
-                                )
-                            }
-                        }
-                        Spacer(Modifier.preferredHeight(26.dp))
                     }
+                    Spacer(Modifier.preferredHeight(16.dp))
+                    TextButton(onClick = {
+                        authenticationModeToggled()
+                    }) {
+                        ProvideTextStyle(value = TextStyle(textAlign = TextAlign.Center)) {
+                            Text(
+                                text = if (authenticationMode == AuthenticateMode.SignIn) {
+                                    stringResource(R.string.no_account)
+                                } else {
+                                    stringResource(R.string.existing_account)
+                                },
+                                modifier = Modifier.preferredSizeIn(minWidth = 220.dp)
+                            )
+                        }
+                    }
+                    Spacer(Modifier.preferredHeight(26.dp))
                 }
                 val showingDialog = state { errorMessage }
                 if (showingDialog.value != null) {
