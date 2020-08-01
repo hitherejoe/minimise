3bd6cc2ef982670d6d94c081325a054dc53d10f0
Joe Birch <hitherejoebirch@googlemail.com>
2020-08-01 06:58:52 +0100

    Create create_android_pr_ui_commands.sh

---
+++ .github/scripts/create_android_pr_ui_commands.sh
@@ -0,0 +1,27 @@
+set -e
+
+git fetch --unshallow
+git fetch origin
+
+# Get all the modules that were changed
+while read line; do
+  module_name=${line%%/*}
+  if [[ ${MODULES} != *"${module_name}"* ]]; then
+    MODULES="${MODULES} ${module_name}"
+  fi
+done < <(git diff --name-only origin/$GITHUB_BASE_REF)
+changed_modules=$MODULES
+
+# Get a list of all available gradle tasks
+AVAILABLE_TASKS=$(./gradlew tasks --all)
+
+# Check if these modules have gradle tasks
+build_commands=""
+for module in $changed_modules
+do
+  if [[ $AVAILABLE_TASKS =~ ":platform_android"$module":connectedCheck" ]]; then
+    build_commands=${build_commands}" "${module}":platform_android:connectedCheck
+  fi
+done
+
+eval "./gradlew ${build_commands}"
