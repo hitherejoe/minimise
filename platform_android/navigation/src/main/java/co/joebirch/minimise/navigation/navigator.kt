267d4ee6c57de50d22a374ebb73da19ecf0bd2ce
hitherejoe <hitherejoebirch@gmail.com>
2021-01-31 16:34:10 +0000

    Remove unused navigator composable

--- platform_android/navigation/src/main/java/co/joebirch/minimise/navigation/navigator.kt
+++
@@ -1,9 +0,0 @@
-package co.joebirch.minimise.navigation
-
-import androidx.compose.runtime.Composable
-import androidx.navigation.NavController
-
-@Composable
-fun Navigator(navController: NavController) {
-    //navController.navigate()
-}
